51931626d62bd7d2c53d07a073c14e5b9ba41c90
shakibn2004 <shakibn2004@gmail.com>
2026-02-22 23:45:30 +0600

    button toggling and empty card arive

--- job-traker.js
+++ job-traker.js
@@ -1,36 +1,76 @@
-
 let totalJobCollection = getElement("job-cards").children;
-console.log(totalJobCollection);
-let totalInterviewCollection = [1, 4];
-let totalRejectedCollection = [1];
-
+let totalInterviewCollection = [0];
+let totalRejectedCollection = [];
 
 getElement("total-jobs").innerText = totalJobCollection.length;
 getElement("interview-jobs").innerText = totalInterviewCollection.length;
 getElement("rejected-jobs").innerText = totalRejectedCollection.length;
 
-
-
-
 // button toggle
 
 function toggleStyle(id) {
-    if(id === "menu-item-all") {
-        getElement("menu-item-all").classList.add("menu-toggle");
-        getElement("menu-item-interview").classList.remove("menu-toggle");
-        getElement("menu-item-rejected").classList.remove("menu-toggle");
+  if (id === "menu-item-all") {
+    getElement("menu-item-all").classList.add("menu-toggle");
+    getElement("menu-item-interview").classList.remove("menu-toggle");
+    getElement("menu-item-rejected").classList.remove("menu-toggle");
 
+    function addAllJobs() {
+      for (let i = 0; i < totalJobCollection.length; i++) {
+        totalJobCollection[i].classList.remove("hidden");
+      }
     }
-    else if(id === "menu-item-interview") {
-        getElement("menu-item-all").classList.remove("menu-toggle");
-        getElement("menu-item-interview").classList.add("menu-toggle");
-        getElement("menu-item-rejected").classList.remove("menu-toggle");
+    addAllJobs();
+  } else if (id === "menu-item-interview") {
+    getElement("menu-item-all").classList.remove("menu-toggle");
+    getElement("menu-item-interview").classList.add("menu-toggle");
+    getElement("menu-item-rejected").classList.remove("menu-toggle");
 
-    }
-    else if(id === "menu-item-rejected") {
-        getElement("menu-item-all").classList.remove("menu-toggle");
-        getElement("menu-item-interview").classList.remove("menu-toggle");
-        getElement("menu-item-rejected").classList.add("menu-toggle");
+    function removeAllJobs() {
+      for (let i = 0; i < totalJobCollection.length; i++) {
+        totalJobCollection[i].classList.add("hidden");
+      }
 
+      if (totalInterviewCollection.length == 0) {
+        let emtyJob = getElement("emty-job");
+        let interviewContainer = getElement("job-cards");
+        interviewContainer.append(emtyJob);
+        emtyJob.style.display = "block";
+      } else {
+        getElement("emty-job").style.display = "none";
+        for (const item of totalInterviewCollection) {
+        }
+
+      }
     }
+
+    removeAllJobs();
+
+    function interviewCount() {}
+    interviewCount();
+  } else if (id === "menu-item-rejected") {
+    getElement("menu-item-all").classList.remove("menu-toggle");
+    getElement("menu-item-interview").classList.remove("menu-toggle");
+    getElement("menu-item-rejected").classList.add("menu-toggle");
+
+    function removeAllJobs() {
+      for (let i = 0; i < totalJobCollection.length; i++) {
+        totalJobCollection[i].classList.add("hidden");
+      }
+
+      if (totalRejectedCollection.length == 0) {
+        let emtyJob = getElement("emty-job");
+        let rejectedContainer = getElement("job-cards");
+        rejectedContainer.append(emtyJob);
+        emtyJob.style.display = "block";
+      } else {
+        getElement("emty-job").style.display = "none";
+        for (const item of totalRejectedCollection) {
+            
+        }
+        
+      }
+    }
+
+    removeAllJobs();
+  }
 }
